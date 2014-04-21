e169ecfcedc60c1b1508e4494aa2d1b4cb7f3b30
Tomasz Lipinski <developer@riffdiktator.com>
2014-04-21 21:46:42 +0200

    Konfiguracja bazy i nawiazanie polaczenia

--- app.js
+++ app.js
@@ -8,7 +8,27 @@
 var express          = require('express'),
     path             = require('path'),
     mainPage         = require('./routes/index'),
-    page             = require('./routes/page');
+    page             = require('./routes/page'),
+    mysql            = require('mysql');
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'nodejs'
+});
+connection.connect();
+
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
 
 var app = express();
 
