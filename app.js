3058cca0da4ec70389e6332779fbf33bedecf3b3
Tomasz Lipinski <developer@riffdiktator.com>
2014-04-21 22:09:27 +0200

    Wybranie tabeli i sprawdzenie dlugosci wynikow

--- app.js
+++ app.js
@@ -12,23 +12,12 @@ var express          = require('express'),
     mysql            = require('mysql');
 
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'nodejs'
-});
-connection.connect();
-
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
+var globalConf = {
+  "host" : "localhost",
+  "username" : "root",
+  "password" : "root",
+  "database" : "nodejs"
+}
 
 var app = express();
 
--- routes/page.js
+++ routes/page.js
@@ -7,5 +7,25 @@ exports.show = function(req, res) {
 }
 
 exports.edit = function(req, res) {
+  
+  var mysql = require('mysql');
+  
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'nodejs'
+  });
+  connection.connect();
+
+
+  connection.query('SELECT * FROM users', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('Ilość wyników: ', rows.length);
+  });
+
+  connection.end();
+
   res.render('page', {title : "Akcja na podstronie", id : req.params.id, action : req.params.action});
 }
